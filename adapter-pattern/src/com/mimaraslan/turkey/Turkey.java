1a13b6d73d2dd9e688fa122af0ceaaca3c64eb06
mimaraslan <mimaraslan@gmail.com>
2020-04-11 14:14:16 +0300

    Adapter Pattern
    Adapter Pattern

--- adapter-pattern/src/com/mimaraslan/turkey/Turkey.java
+++ adapter-pattern/src/com/mimaraslan/turkey/Turkey.java
@@ -2,5 +2,6 @@ package com.mimaraslan.turkey;
 
 public interface Turkey {
 	public void gobble();
+
 	public void fly();
 }
---
+++ adapter-pattern2/src/com/mimaraslan/EnumerationIteratorTestDrive.java
@@ -0,0 +1,13 @@
+package com.mimaraslan;
+
+import java.util.*;
+
+public class EnumerationIteratorTestDrive {
+	public static void main(String args[]) {
+		Vector v = new Vector(Arrays.asList(args));
+		Iterator iterator = new EnumerationIterator(v.elements());
+		while (iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
+	}
+}
